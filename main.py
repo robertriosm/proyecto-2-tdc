9aea97dffdb63d999f931a520c11a5c078515b9c
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-11-22 20:07:46 -0600

    fixed gramatic declaration

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from Gramatica import Gramatica
 gram = Gramatica(
-    V = {'S', 'NP', 'VP', 'PP', 'V', 'P', 'N', 'Det'}, 
+    V = {'S', 'A', 'B', 'C', 'V', 'P', 'N', 'D'}, 
     T = {'cooks', 'drinks', 'eats', 'cuts', 'he', 'she', 'in', 'with', 'cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon', 'a', 'the'}, 
     S = 'S', 
     P=[
-        ('S', [(0, ('NP', 'VP'))], 0),
-        ('VP', [(0, ('VP', 'PP')), (0, ('V', 'VP')), (1, ['cooks', 'drinks', 'eats', 'cuts'])], 2),
-        ('PP', [(0, ('P', 'NP'))], 0),
-        ('NP', [(0, ('Det', 'N')), (1, ['he', 'she'])], 2),
+        ('S', [(0, ('A', 'B'))], 0),
+        ('B', [(0, ('B', 'C')), (0, ('V', 'A')), (1, ['cooks', 'drinks', 'eats', 'cuts'])], 2),
+        ('C', [(0, ('P', 'A'))], 0),
+        ('A', [(0, ('D', 'N')), (1, ['he', 'she'])], 2),
         ('V', [(1, ['cooks', 'drinks', 'eats', 'cuts'])], 1),
         ('P', [(1, ['in', 'with'])], 1),
         ('N', [(1, ['cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon'])], 1),
-        ('Det', [(1, ['a', 'the'])], 1)
+        ('D', [(1, ['a', 'the'])], 1)
     ]) 
-print(gram.testWord('he drinks with he'))
+print(gram.testWord('She eats a cake with a fork'))
