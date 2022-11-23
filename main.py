58b79fe253e7c0a7bf3689490950b62c0613117f
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-11-22 19:25:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ gram = Gramatica(
     T = {'cooks', 'drinks', 'eats', 'cuts', 'he', 'she', 'in', 'with', 'cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon', 'a', 'the'}, 
     S = 'S', 
     P=[
-        ('S', ('NP', 'VP'), 0),
-        ('VP', ('VP', 'PP'), 0),
-        ('VP', ('V', 'VP'), 0),
-        ('VP', ['cooks', 'drinks', 'eats', 'cuts'], 1),
-        ('PP', ('P', 'NP'), 0),
-    ])
+        ('S', [(0, ('NP', 'VP'))], 0),
+        ('VP', [(0, ('VP', 'PP')), (0, ('V', 'VP')), (1, ['cooks', 'drinks', 'eats', 'cuts'])], 2),
+        ('PP', [(0, ('P', 'NP'))], 0),
+        ('NP', [(0, ('Det', 'N')), (1, ['he', 'she'])], 2),
+        ('V', [(1, ['cooks', 'drinks', 'eats', 'cuts'])], 1),
+        ('P', [(1, ['in', 'with'])], 1),
+        ('N', [(1, ['cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon'])], 1),
+        ('Det', [(1, ['a', 'the'])], 1)
+    ]) 
+print(gram.testWord('he drinks with he'))
